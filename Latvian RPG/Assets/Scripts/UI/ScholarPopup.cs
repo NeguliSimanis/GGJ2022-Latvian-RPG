b54eed8ce66c84c83d416ac7efbf06d816d7ce96
NeguliSimanis <simanism@inbox.lv>
2022-03-26 13:51:23 +0200

    scholar progress

---
+++ Latvian RPG/Assets/Scripts/UI/ScholarPopup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScholarPopup : MonoBehaviour
+{
+
+    public Text skillText;
+    public Text scholarText;
+
+    [Header("Light option")]
+    public Button lightOptionButton;
+    public Text lightBigText;
+    public Text lightSmallText;
+
+    [Header("Dark option")]
+    public Button darkOptionButton;
+    public Text darkBigText;
+    public Text darkSmallText;
+}
