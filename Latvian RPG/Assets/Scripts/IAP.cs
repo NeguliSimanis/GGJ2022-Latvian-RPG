6e94b78eff09cb3d4143377318579b1aa86f4eea
NeguliSimanis <simanism@inbox.lv>
2022-07-24 12:17:09 +0300

    Revert "iAP problems"
    This reverts commit 79b0e426740ae5f8ff172692ee181c7443fd5cb5.

--- Latvian RPG/Assets/Scripts/IAP.cs
+++ Latvian RPG/Assets/Scripts/IAP.cs
@@ -2,29 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Purchasing;
-using UnityEngine.Purchasing.Extension;
-
-/// <summary>
-/// tutorial: https://zditect.com/code/android/use-the-google-play-billing-library-with-unity-nbspnbsp-google-plays-billing-system-nbspnbsp-android-developers.html
-/// forum post: https://github.com/google/play-unity-plugins/issues/52
-/// yt video: https://www.youtube.com/watch?v=QKyLkLjr46k&ab_channel=GleyGames
-/// </summary>
 
 public class IAP : MonoBehaviour
 {
     private string fullVersion = "com.simanismikoss.moonaria.fullversion";
     [SerializeField]
     PopupManager popupManager;
-
-    ConfigurationBuilder builder;
-
-    private void Awake()
-    {
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            builder = ConfigurationBuilder.Instance(Google.Play.Billing.GooglePlayStoreModule.Instance());
-        }
-    }
 
     public void OnPurchaseComplete(Product product)
     {
